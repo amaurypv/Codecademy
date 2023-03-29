0f001b36dabe420aedd523546b048f56042a3de0
amaury perez verdejo <amauryperezverdejo@MacBook-Air-de-amaury.local>
2023-03-29 14:23:25 -0600

    ssa 332

---
+++ js/cotizaciones.js
@@ -0,0 +1,16 @@
+let alcohol=1.19
+let acetona=1.2 
+let cabosilm5=20.85
+let cabosilts610=32.05
+let cabosilts720=31.78
+
+const cotizacion=(costo,utilidad)=>{
+    return costo/(1-(utilidad*.01))
+}
+
+const calcutilidad=(costo,venta)=>{
+    let utilidad=((venta-costo)/venta)
+    return utilidad*100
+}
+
+console.log(calcutilidad(1.29,1.74))
