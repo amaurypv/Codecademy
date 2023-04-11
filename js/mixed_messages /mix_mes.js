08c5c42dd13a66d25b822c197ffaa3bc3e2510d2
amaurypv <amaurypv@gmail.com>
2023-04-11 11:29:21 -0600

    final attemp

--- js/mixed_messages /mix_mes.js
+++ js/mixed_messages /mix_mes.js
@@ -1,3 +1,4 @@
+//first define an array with 10 sentences about weater
 let sentences=['The sun is shining brightly and the sky is clear, making for a beautiful day.',
 'Its been raining all morning, and the streets are starting to flood',
 'The wind is picking up, and it looks like a storm might be on the way.',
@@ -8,10 +9,11 @@ let sentences=['The sun is shining brightly and the sky is clear, making for a b
 'The weather is quite mild today, with a comfortable temperature and moderate humidity.',
 'Theres a thick layer of fog blanketing the city, making it difficult to see more than a few feet ahead.',
 'Its a perfect day for a picnic, with plenty of sunshine and a gentle breeze to keep things cool']
-
-let nuram=Math.floor(Math.random()*10)
+//define a number random between the length of the array 
+let nuram=Math.floor(Math.random()*((sentences.length)+1))
+//made a function that prints a sentence random
 const rand=()=>{
     console.log(sentences[nuram])
 }
-
+//call the function
 rand()
